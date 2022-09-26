8688dc9293fb0c9e74981e527be5246d810e9a47
Akylbek <ashyrbaew@gmail.com>
2022-09-26 11:08:51 +0600

    test

--- main/models.py
+++ main/models.py
@@ -2,25 +2,19 @@ from django.db import models
 from django.conf import settings
 
 
-class TodoItem(models.Model):
-    """
-    Todo Item Model
-    """
-    name = models.CharField(max_length=100)
+class Urls(models.Model):
+    description = models.CharField(max_length=100)
+    url = models.CharField(max_length=400, null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    is_completed = models.BooleanField(default=False)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-        related_name="todo_item")
+    is_deleted = models.BooleanField(default=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,
+        related_name="url_item")
 
     class Meta:
-        """
-        Meta Information
-        """
-        app_label = "todo"
-        db_table = "todo_item"
-        verbose_name = "todo_item"
-        verbose_name_plural = "todo_items"
+        app_label = "urls"
+        verbose_name = "url_item"
+        verbose_name_plural = "url_items"
 
     def __str__(self):
-        return self.name
+        return self.description
